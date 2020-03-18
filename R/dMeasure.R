ebb5dd2bbc4ef3f2bb13f65c7c17678110470198
David Fong <vkelim@bigpond.com>
2020-03-18 11:50:34 +1100

    define set_reactive directly

--- R/dMeasure.R
+++ R/dMeasure.R
@@ -76,15 +76,26 @@ dMeasure <-
 
 ##### special reactive functions ##########################
 
-.private(dMeasure, "set_reactive", function(myreactive, value) {
-  # reactive (if shiny/reactive environment is available) is set to 'value'
-  # myreactive is passed by reference
-  # print(myreactive)
-  # print(deparse(sys.call(-1)))
-  if (requireNamespace("shiny", quietly = TRUE) && shiny::is.reactive(myreactive)) {
-    myreactive(value)
-  }
-})
+dMeasure$set("private", "set_reactive",
+             function(myreactive, value) {
+               # reactive (if shiny/reactive environment is available) is set to 'value'
+               # myreactive is passed by reference
+               # print(myreactive)
+               # print(deparse(sys.call(-1)))
+               if (requireNamespace("shiny", quietly = TRUE) && shiny::is.reactive(myreactive)) {
+                 myreactive(value)
+               }
+             })
+
+# .private(dMeasure, "set_reactive", function(myreactive, value) {
+#   # reactive (if shiny/reactive environment is available) is set to 'value'
+#   # myreactive is passed by reference
+#   # print(myreactive)
+#   # print(deparse(sys.call(-1)))
+#   if (requireNamespace("shiny", quietly = TRUE) && shiny::is.reactive(myreactive)) {
+#     myreactive(value)
+#   }
+# })
 .private(dMeasure, "trigger", function(myreactive) {
   # toggles a reactive between (usually) 0 and 1
   if (requireNamespace("shiny", quietly = TRUE)) {
